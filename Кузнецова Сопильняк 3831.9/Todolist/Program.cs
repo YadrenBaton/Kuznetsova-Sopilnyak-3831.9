7c2f8ff4e0e8dbdbf89d9ab7b089a65fee57f4c4
YadrenBaton <grifon423@gmail.com>
2025-10-03 18:39:13 +0300

    Check

--- Кузнецова Сопильняк 3831.9/Todolist/Program.cs
+++ Кузнецова Сопильняк 3831.9/Todolist/Program.cs
@@ -10,13 +10,13 @@ class Program
         // Запрос данных у пользователя
         Console.Write("Введите имя: ");
         string name = Console.ReadLine();
-        
+
         Console.Write("Введите фамилию: ");
         string surname = Console.ReadLine();
-        
+
         Console.Write("Введите год рождения: ");
         string yearInput = Console.ReadLine();
-        
+
         // Преобразование года рождения в число
         int birthYear;
         while (!int.TryParse(yearInput, out birthYear))
@@ -24,14 +24,16 @@ class Program
             Console.Write("Ошибка! Введите корректный год рождения: ");
             yearInput = Console.ReadLine();
         }
-        
+
         // Получение текущего года
         int currentYear = DateTime.Now.Year;
-        
+
         // Вычисление возраста
         int age = currentYear - birthYear;
-        
+
         // Вывод результата
         Console.WriteLine($"Добро пожаловать пользователь {name} {surname}, возраст - {age}");
     }
 }
+//111
+
